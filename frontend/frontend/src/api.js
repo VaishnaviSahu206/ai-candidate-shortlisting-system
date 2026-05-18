794237f9da77085ea8068f428304d685a20f118e
VaishnaviSahu206 <vaishnavi.2428cseaim@kiet.edu>
2026-05-18 12:39:44 +0530

    Fixed missing api route

--- frontend/frontend/src/api.js
+++ frontend/frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://ai-candidate-shortlisting-system-y0f9.onrender.com"
+    baseURL: "https://ai-candidate-shortlisting-system-y0f9.onrender.com/api"
 });
